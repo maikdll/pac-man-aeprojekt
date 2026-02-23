4a106b6feb6521462951789080023985e6f72730
Niko Reichel <niko.reichel@stackit.cloud>
2026-02-23 10:14:03 +0100

    pacmanfunktioniert

--- pac-man-ae-prjekt/scripts/PacMan.gd
+++ pac-man-ae-prjekt/scripts/PacMan.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var current_direction = Vector2.RIGHT
 
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("ui_right"):
+		current_direction = Vector2.RIGHT
+	elif Input.is_action_pressed("ui_left"):
+		current_direction = Vector2.LEFT
+	elif Input.is_action_pressed("ui_down"):
+		current_direction = Vector2.DOWN
+	elif Input.is_action_pressed("ui_up"):
+		current_direction = Vector2.UP
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	velocity = current_direction * SPEED
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	# 3. Bewegen und an Wänden sliden
 	move_and_slide()
+	
+	rotation = current_direction.angle()
