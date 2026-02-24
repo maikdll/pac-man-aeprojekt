ac68909ea2b35517521e74e11c82ae16e995f950
Niko Reichel <niko.reichel@stackit.cloud>
2026-02-24 14:56:55 +0100

    lot stuff

--- pac-man-ae-prjekt/scripts/healthbar.gd
+++ pac-man-ae-prjekt/scripts/healthbar.gd
@@ -3,10 +3,13 @@ extends Control
 @onready var life_icons = $HBoxContainer.get_children()
 
 func _ready():
+	Global.health = 3
 	update_ui(Global.health)
 
 func update_ui(current_health):
 	if Global.health < 1:
 		$AudioDeath.play()
+		get_tree().call_group("GameOver", "setGameOverLabel")
+		
 	for i in range(life_icons.size()):
 		life_icons[i].visible = i < current_health
---
+++ pac-man-ae-prjekt/scripts/start_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var highscore_label = $VBoxContainer/HighscoreLabel
+
+func _on_button_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
+func _ready():
+	display_leaderboard()
+
+func display_leaderboard():
+	var text = "TOP 5 HIGHSCORES\n"
+	text += "----------------\n"
+	
+	for i in range(Global.leaderboard.size()):
+		var entry = Global.leaderboard[i]
+		text += str(i + 1) + ". " + entry["name"] + ": " + str(entry["score"]) + "\n"
+	
+	highscore_label.text = text
