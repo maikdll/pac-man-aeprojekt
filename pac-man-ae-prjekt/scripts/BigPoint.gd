0d72f1b9203c6927d0880a488d38624d907c48c6
Niko Reichel <niko.reichel@stackit.cloud>
2026-03-05 16:25:40 +0100

    blinking big points

--- pac-man-ae-prjekt/scripts/BigPoint.gd
+++ pac-man-ae-prjekt/scripts/BigPoint.gd
@@ -1,5 +1,17 @@
 extends Area2D
 
+@onready var sprite = $Sprite2D 
+
+func _ready():
+	var blink_timer = Timer.new()
+	blink_timer.wait_time = 0.25 
+	blink_timer.autostart = true
+	blink_timer.timeout.connect(_on_blink_timer_timeout)
+	add_child(blink_timer)
+
+func _on_blink_timer_timeout():
+	if sprite != null:
+		sprite.visible = not sprite.visible
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "PacMan":
