f675d6b4597813117b5b6c9c896f6dbe92b8995c
Niko Reichel <niko.reichel@stackit.cloud>
2026-03-09 16:29:15 +0100

    working Websocket

--- pac-man-ae-prjekt/scripts/WebSocketPeer.gd
+++ pac-man-ae-prjekt/scripts/WebSocketPeer.gd
@@ -1,11 +1,10 @@
 extends Node
 
 var socket = WebSocketPeer.new()
-
-var url = "ws://DEINE_SERVER_IP_HIER" 
+var url = "ws://localhost:8765"
 
 func _ready():
-	print("Verbinde mit LED-Server...")
+	print("Verbinde mit LED-Bridge...")
 	var err = socket.connect_to_url(url)
 	if err != OK:
 		print("Fehler beim Verbindungsaufbau: ", err)
@@ -14,23 +13,43 @@ func _process(_delta):
 	socket.poll()
 	var state = socket.get_ready_state()
 	
-	if state == WebSocketPeer.STATE_CLOSED:
+	if state == WebSocketPeer.STATE_OPEN:
+		while socket.get_available_packet_count() > 0:
+			socket.get_packet() 
+			
+	elif state == WebSocketPeer.STATE_CLOSED:
 		var code = socket.get_close_code()
 		var reason = socket.get_close_reason()
 		print("Verbindung geschlossen. Code: ", code, " Grund: ", reason)
 		set_process(false) 
 
-func send_led_color(color: Color):
+func send_effect(chain: String, effect_type: String, color: Color, segment: int = 99):
 	if socket.get_ready_state() == WebSocketPeer.STATE_OPEN:
-		var data = {
-			"r": int(color.r * 255),
-			"g": int(color.g * 255),
-			"b": int(color.b * 255)
-		}
-		var json_string = JSON.stringify(data)
 		
-		socket.put_packet(json_string.to_utf8_buffer())
-		print("Farbe gesendet: ", json_string)
+		var data = {
+			"cmd": "effect",
+			"chain": chain,
+			"type": effect_type,
+			"segment": segment, 
+			"color": {
+				"r": int(color.r * 255),
+				"g": int(color.g * 255),
+				"b": int(color.b * 255)
+			},
+			"speed": 50,
+			"repeat": 1,
+			"priority": 2
+		}
+		
+		var json_string = JSON.stringify(data)
+		socket.send_text(json_string)
+		
+		print("Befehl gesendet: ", json_string)
 		
 	else:
-		print("Senden fehlgeschlagen: Keine offene Verbindung.")
+		print("Senden fehlgeschlagen: Websocket noch nicht verbunden.")
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"): 
+		print("Leertaste gedrückt! Feuere Effekt ab...")
+		send_effect("A", "chase", Color.RED)
